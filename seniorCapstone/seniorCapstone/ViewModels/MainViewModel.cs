1d9a6bb3cd07b4db6bc2ce63c8424b4f8d0c3cb7
Audrey Lincoln <audrey1146@gmail.com>
2020-10-21 17:08:25 -0700

    Set up

---
+++ seniorCapstone/seniorCapstone/ViewModels/MainViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace seniorCapstone.ViewModels
+{
+	/// <summary>
+	/// Viewmodel class that implements the properties and commands for the
+	/// main page of the app.
+	/// </summary>
+	public class MainViewModel : INotifyPropertyChanged
+	{
+		// Variables
+		public event PropertyChangedEventHandler PropertyChanged;
+
+		/// <summary>
+		/// 
+		/// </summary>
+		public void OnPropertyChanged ()
+		{
+
+		}
+	}
+}
