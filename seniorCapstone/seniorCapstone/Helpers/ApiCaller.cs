33bc46b1006c2a95d0bd7f1773322329467b27e4
Audrey Lincoln <audrey1146@gmail.com>
2021-02-20 13:21:04 -0800

    Adding weatherAPI

---
+++ seniorCapstone/seniorCapstone/Helpers/ApiCaller.cs
@@ -0,0 +1,52 @@
+﻿/****************************************************************************
+ * File			ApiCaller.cs
+ * Author		Audrey Lincoln
+ * Date			2/20/2021
+ * Purpose		Helper class to get the response of the Api
+ ****************************************************************************/
+
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace seniorCapstone.Helpers
+{
+    public class ApiCaller
+    {
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="url"></param>
+        /// <param name="authId"></param>
+        /// <returns></returns>
+        public static async Task<ApiResponse> Get (string url, string authId = null)
+        {
+            using (var client = new HttpClient ())
+            {
+                if (!string.IsNullOrWhiteSpace (authId))
+				{
+                    client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue ("Authorization", authId);
+                }
+                    
+                var request = await client.GetAsync (url);
+                if (request.IsSuccessStatusCode)
+                {
+                    return new ApiResponse { Response = await request.Content.ReadAsStringAsync () };
+                }
+                else
+                    return new ApiResponse { ErrorMessage = request.ReasonPhrase };
+            }
+        }
+    }
+
+    public class ApiResponse
+    {
+        public bool Successful => ErrorMessage == null;
+        public string ErrorMessage { get; set; }
+        public string Response { get; set; }
+    }
+}
--- seniorCapstone/seniorCapstone/Views/EditAccountPopupPage.xaml.cs
+++ seniorCapstone/seniorCapstone/Views/EditAccountPopupPage.xaml.cs
@@ -1,16 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿using Rg.Plugins.Popup.Pages;
+using Rg.Plugins.Popup.Services;
+using System;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 namespace seniorCapstone.Views
 {
 	[XamlCompilation (XamlCompilationOptions.Compile)]
-	public partial class EditAccountPopupPage : ContentPage
+	public partial class EditAccountPopupPage : PopupPage
 	{
 		public EditAccountPopupPage ()
 		{
