d348c523eaa7ff05f7a5477520a0ff11d1cc6714
Audrey Lincoln <audrey1146@gmail.com>
2021-03-14 21:38:49 -0700

    Soil type stuf??

--- seniorCapstone/seniorCapstone/Helpers/RainCat.cs
+++ seniorCapstone/seniorCapstone/Helpers/RainCat.cs
@@ -1,16 +1,41 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using seniorCapstone.Helpers;
 
 namespace seniorCapstone.Helpers
 {
 	public class RainCat
 	{
+		private SoilType mcSoilType;
 
 		public RainCat ()
 		{
 
 		}
 
+		/// <summary>
+		/// 
+		/// </summary>
+		public float InstantaneousInfiltrationRate (string soil)
+		{
+			float IR = 0;
+
+			switch (soil)
+			{
+				case (SoilType.SAND):
+					break;
+				case (SoilType.SANDY_LOAM):
+					break;
+				case (SoilType.CLAY):
+					break;
+				case (SoilType.CLAY_LOAM):
+					break;
+				case (SoilType.LOAM):
+					break;
+			}
+
+			return (IR);
+		}
 	}
 }
